29f9fcf9aa26bcdc40ffbed892a6e5fef02e111a
Josip Horvat <josiph988@gmail.com>
2020-10-20 00:37:44 +0200

    Dodan Entitet koji je id i primary key te Gost.

---
+++ src/main/java/hr/horvat/model/Entitet.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hr.horvat.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+/**
+ *
+ * @author Josip
+ */
+
+@MappedSuperclass
+public class Entitet {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+    
+    
+}
--- src/main/java/hr/horvat/model/Gost.java
+++ src/main/java/hr/horvat/model/Gost.java
@@ -5,10 +5,78 @@
  */
 package hr.horvat.model;
 
+import java.util.Date;
+import javax.persistence.Entity;
+
 /**
  *
  * @author Josip
  */
-public class Gost {
+@Entity
+public class Gost extends Entitet{
+    
+    private String ime;
+    private String prezime;
+    private String email;
+    private String oib;
+    private Date datumRodjenja;
+    private String drzava;
+    private String grad;
+
+    public String getDrzava() {
+        return drzava;
+    }
+
+    public void setDrzava(String drzava) {
+        this.drzava = drzava;
+    }
+
+    public String getGrad() {
+        return grad;
+    }
+
+    public void setGrad(String grad) {
+        this.grad = grad;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getOib() {
+        return oib;
+    }
+
+    public void setOib(String oib) {
+        this.oib = oib;
+    }
+
+    public Date getDatumRodjenja() {
+        return datumRodjenja;
+    }
+
+    public void setDatumRodjenja(Date datumRodjenja) {
+        this.datumRodjenja = datumRodjenja;
+    }
     
 }
